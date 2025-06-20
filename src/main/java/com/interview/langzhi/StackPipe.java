6b42cb7279191c73a50a066bb35e27f8b09766cc
cnyangkuo <80479235@qq.com>
2025-06-20 09:28:42 +0800

    add StackPipe

--- src/main/java/com/interview/langzhi/StackPipe.java
+++ src/main/java/com/interview/langzhi/StackPipe.java
@@ -1,20 +1,30 @@
 package com.interview.langzhi;
 
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * 栈型队列类
  * @author yangkuo
  * @date 2025/6/19
- * @description
+ * @description 实现Iterable接口以支持迭代器
  */
-public class StackPipe<T> {
+public class StackPipe<T> implements Iterable<T> {
     // 栈顶指针
     private Node<T> top;
     // 栈底指针
     private Node<T> bottom;
+    // 新增计数器
+    private int size;
+    // 锁
+    private final Lock lock = new ReentrantLock();
 
     public StackPipe() {
         this.top = null;
         this.bottom = null;
+        this.size = 0;
     }
 
     /**
@@ -22,14 +32,20 @@ public class StackPipe<T> {
      * @param data
      */
     public void push(T data) {
-        Node<T> newNode = new Node<>(data);
-        if (top == null) {
-            top = newNode;
-            bottom = newNode;
-        } else {
-            newNode.setDown(top);
-            top.setUp(newNode);
-            top = newNode;
+        lock.lock();
+        try {
+            Node<T> newNode = new Node<>(data);
+            if (top == null) {
+                top = newNode;
+                bottom = newNode;
+            } else {
+                newNode.setDown(top);
+                top.setUp(newNode);
+                top = newNode;
+            }
+            size++;
+        } finally {
+            lock.unlock();
         }
     }
 
@@ -38,19 +54,25 @@ public class StackPipe<T> {
      * @return
      */
     public T pop() {
-        if (top == null) {
-            return null;
+        lock.lock();
+        try {
+            if (top == null) {
+                return null;
+            }
+            T data = top.getData();
+            // 栈顶指针下移
+            top = top.getDown();
+            if (top != null) {
+                // 栈顶元素移除
+                top.setUp(null);
+            } else {
+                bottom = null;
+            }
+            size--;
+            return data;
+        } finally {
+            lock.unlock();
         }
-        T data = top.getData();
-        // 栈顶指针下移
-        top = top.getDown();
-        if (top != null) {
-            // 栈顶元素移除
-            top.setUp(null);
-        } else {
-            bottom = null;
-        }
-        return data;
     }
 
     /**
@@ -58,19 +80,82 @@ public class StackPipe<T> {
      * @return
      */
     public T pop2() {
-        if (bottom == null) {
-            return null;
+        lock.lock();
+        try {
+            if (bottom == null) {
+                return null;
+            }
+            T data = bottom.getData();
+            // 栈底指针上移
+            bottom = bottom.getUp();
+            if (bottom != null) {
+                // 栈底元素移除
+                bottom.setDown(null);
+            } else {
+                top = null;
+            }
+            size--;
+            return data;
+        } finally {
+            lock.unlock();
         }
-        T data = bottom.getData();
-        // 栈底指针上移
-        bottom = bottom.getUp();
-        if (bottom != null) {
-            // 栈底元素移除
-            bottom.setDown(null);
-        } else {
-            top = null;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /**
+     * 栈翻转
+     */
+    public void reverse(){
+        lock.lock();
+        try {
+            if (size() <= 1) {
+                return;
+            }
+            Node<T> oldTop = top;
+            Node<T> oldBottom = bottom;
+
+            Node<T> current = top;
+            while (current != null) {
+                Node<T> next = current.getDown();
+                current.setDown(current.getUp());
+                current.setUp(next);
+                current = next;
+            }
+            top = oldBottom;
+            bottom = oldTop;
+        } finally {
+            lock.unlock();
         }
-        return data;
+    }
+
+    // 查找中间节点（快慢指针法）
+    public T findMiddle() {
+        lock.lock();
+        try {
+            if (size() < 1) {
+                return null;
+            }
+
+            Node<T> slow = top;
+            Node<T> fast = top;
+
+            while (fast != null && fast.getDown() != null) {
+                slow = slow.getDown();
+                fast = fast.getDown().getDown();
+            }
+
+            return slow.getData();
+        } finally {
+            lock.unlock();
+        }
+
     }
 
     /**
@@ -84,6 +169,33 @@ public class StackPipe<T> {
         System.out.println();
     }
 
+    @Override
+    public Iterator<T> iterator() {
+        return new StackIterator();
+    }
+
+    /**
+     * 迭代器实现类
+     */
+    private class StackIterator implements Iterator<T> {
+        private Node<T> current = top;
+
+        @Override
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        @Override
+        public T next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException("Stack is empty");
+            }
+            T data = current.getData();
+            current = current.getDown();
+            return data;
+        }
+    }
+
     public static void main(String[] args) {
         StackPipe<Integer> stackPipe = new StackPipe<>();
 
@@ -91,6 +203,24 @@ public class StackPipe<T> {
         stackPipe.push(1);
         stackPipe.push(2);
         stackPipe.push(3);
+
+        System.out.println("===== 使用迭代器打印栈内容 =====");
+        for (Integer num : stackPipe) {
+            System.out.println(num);
+        }
+        System.out.println();
+
+        System.out.println("===== 栈翻转 =====");
+        stackPipe.reverse();
+        stackPipe.printStack();
+
+        System.out.println("===== 栈再次翻转 =====");
+        stackPipe.reverse();
+        stackPipe.printStack();
+
+        System.out.println("===== 查找中间节点 =====");
+        System.out.println("中间节点: " + stackPipe.findMiddle());
+        System.out.println();
 
         System.out.println("===== 打印栈内容(从栈顶到栈底) =====");
         stackPipe.printStack();
