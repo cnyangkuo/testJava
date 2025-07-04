6141b01ccf1fda858f5bbd184c21df1cdc358449
cnyangkuo <80479235@qq.com>
2025-07-04 20:40:00 +0800

    add comment

--- src/main/java/com/interview/sc/StreamTest.java
+++ src/main/java/com/interview/sc/StreamTest.java
@@ -1,9 +1,14 @@
 package com.interview.sc;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
+
 /**
+ * The type Stream test.
+ *
  * @author yangkuo
- * @date 2025/7/4
+ * @date 2025 /7/4
  * @description
  */
 public class StreamTest {
@@ -20,6 +25,17 @@ public class StreamTest {
             return true;
         }).count(); // 终止操作触发执行
 
+        List<Integer> list = Arrays.asList(1, 2, 3);
+        System.out.println("--------------------------------------------------");
+        // 并行输出, 输出顺序不确定
+//        list.parallelStream().forEach(n -> System.out.print(n + " "));  // 带空格
+        list.parallelStream().forEach(System.out::print); // 不带空格
+        System.out.println();
+        System.out.println("--------------------------------------------------");
+        // 顺序输出, 输出顺序一致
+//        list.stream().forEach(n -> System.out.print(n + " "));  // 带空格
+        list.stream().forEach(System.out::print); // 不带空格
+        System.out.println();
     }
 
 }
